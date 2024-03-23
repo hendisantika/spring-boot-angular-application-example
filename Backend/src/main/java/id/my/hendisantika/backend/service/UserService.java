10043fe78004c6879874bc14ea2324ef2d3e5d4a
hendisantika <hendisantika@yahoo.co.id>
2024-03-23 07:59:45 +0700

    Add UserService 🫸🌀✏️📗🐧🐳

---
+++ Backend/src/main/java/id/my/hendisantika/backend/service/UserService.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.backend.service;
+
+import id.my.hendisantika.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-angular-application-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/23/24
+ * Time: 07:59
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+}
