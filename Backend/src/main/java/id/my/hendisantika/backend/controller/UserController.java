7d61fa1848a96c98ffd036cd3744987b6d46ff08
hendisantika <hendisantika@yahoo.co.id>
2024-03-23 08:02:03 +0700

    Add updateUser in UserController 🫸🌀✏️📗🐧🐳

--- Backend/src/main/java/id/my/hendisantika/backend/controller/UserController.java
+++ Backend/src/main/java/id/my/hendisantika/backend/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -48,5 +49,11 @@ public class UserController {
     @ResponseStatus(value = HttpStatus.OK)
     public User findById(@PathVariable int id) {
         return this.userService.findById(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public User updateUser(@PathVariable int id, @RequestBody User userVO) {
+        return this.userService.updateUser(id, userVO);
     }
 }
