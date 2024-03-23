e2f06370dae38d0ea8557c1dd094123cb0c71384
hendisantika <hendisantika@yahoo.co.id>
2024-03-23 08:00:00 +0700

    Add insert in UserService 🫸🌀✏️📗🐧🐳

--- Backend/src/main/java/id/my/hendisantika/backend/service/UserService.java
+++ Backend/src/main/java/id/my/hendisantika/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.backend.service;
 
+import id.my.hendisantika.backend.entity.User;
 import id.my.hendisantika.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,4 +23,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public User insert(User userVO) {
+        return this.userRepository.save(userVO);
+    }
 }
