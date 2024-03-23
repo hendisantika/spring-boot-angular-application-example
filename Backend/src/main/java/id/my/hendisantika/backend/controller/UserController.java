e185e4a4e9c32fc5353d603d7316cacb7a310e64
hendisantika <hendisantika@yahoo.co.id>
2024-03-23 08:01:54 +0700

    Add findById in UserController 🫸🌀✏️📗🐧🐳

--- Backend/src/main/java/id/my/hendisantika/backend/controller/UserController.java
+++ Backend/src/main/java/id/my/hendisantika/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,11 @@ public class UserController {
     @ResponseStatus(value = HttpStatus.OK)
     public List<User> findAllUser() {
         return this.userService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public User findById(@PathVariable int id) {
+        return this.userService.findById(id);
     }
 }
