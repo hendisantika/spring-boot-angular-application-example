d1d44058bc7379ac5e89c46b60b61771afafe04d
hendisantika <hendisantika@yahoo.co.id>
2024-03-23 08:00:12 +0700

    Add findAll in UserService 🫸🌀✏️📗🐧🐳

--- Backend/src/main/java/id/my/hendisantika/backend/service/UserService.java
+++ Backend/src/main/java/id/my/hendisantika/backend/service/UserService.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-angular-application-example
@@ -26,5 +28,9 @@ public class UserService {
 
     public User insert(User userVO) {
         return this.userRepository.save(userVO);
+    }
+
+    public List<User> findAll() {
+        return this.userRepository.findAll();
     }
 }
