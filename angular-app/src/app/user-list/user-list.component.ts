324661c490e71e32be2504ec244251045c1ce150
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 07:14:01 +0700

    Edit user-list.component.ts in Angular App🫸🌀✏️📗🐧🐳

--- angular-app/src/app/user-list/user-list.component.ts
+++ angular-app/src/app/user-list/user-list.component.ts
@@ -1,12 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from '../services/user.service';
+import {User} from '../user.model';
 
 @Component({
   selector: 'app-user-list',
-  standalone: true,
-  imports: [],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[] | undefined;
 
+  constructor(private userService: UserService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  updateUser(id: number) {
+    this.router.navigate(['update-user', id]);
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id).subscribe(data => {
+      console.log(data);
+      this.getUsers();
+    });
+  }
+
+  private getUsers() {
+    this.userService.getUserList().subscribe(data => {
+      this.users = data;
+    });
+  }
 }
