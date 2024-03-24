107daf6a8fa4fe18e4f18d47b950f4acef817d1f
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 07:06:56 +0700

    Modify User Service in Angular App🫸🌀✏️📗🐧🐳

--- angular-app/src/app/services/user.service.ts
+++ angular-app/src/app/services/user.service.ts
@@ -1,10 +1,35 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../user.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() {
+  private basUrl = "http://localhost:8081/user"
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getUserList(): Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.basUrl}`);
+  }
+
+  createUser(user: User): Observable<Object> {
+    return this.httpClient.post(`${this.basUrl}`, user);
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.httpClient.get<User>(`${this.basUrl}/${id}`);
+  }
+
+  updateUser(id: number, user: User): Observable<Object> {
+    return this.httpClient.put(`${this.basUrl}/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<Object> {
+    return this.httpClient.delete(`${this.basUrl}/${id}`);
   }
 }
