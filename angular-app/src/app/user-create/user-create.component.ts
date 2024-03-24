4044ddf8733a8566a462accefad7a55909944ee0
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 07:15:18 +0700

    Edit user-create.component.ts in Angular App🫸🌀✏️📗🐧🐳

--- angular-app/src/app/user-create/user-create.component.ts
+++ angular-app/src/app/user-create/user-create.component.ts
@@ -1,12 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from '../services/user.service';
+import {User} from '../user.model';
 
 @Component({
   selector: 'app-user-create',
-  standalone: true,
-  imports: [],
   templateUrl: './user-create.component.html',
-  styleUrl: './user-create.component.css'
+  styleUrls: ['./user-create.component.css']
 })
-export class UserCreateComponent {
+export class UserCreateComponent implements OnInit {
+  user: User = new User();
 
+  constructor(private userService: UserService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveUser() {
+    this.userService.createUser(this.user).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.redirectToUserList();
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    });
+  }
+
+  redirectToUserList() {
+    this.router.navigate(['/users']);
+  }
+
+  onSubmit() {
+    console.log(this.user);
+    this.saveUser();
+  }
 }
