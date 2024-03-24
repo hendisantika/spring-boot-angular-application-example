5b1577a703cf58756e659c8856cd8b9ae3d6a659
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 07:16:47 +0700

    Edit user-update.component.ts in Angular App🫸🌀✏️📗🐧🐳

--- angular-app/src/app/user-update/user-update.component.ts
+++ angular-app/src/app/user-update/user-update.component.ts
@@ -1,12 +1,55 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../services/user.service';
+import {User} from '../user.model';
 
 @Component({
   selector: 'app-user-update',
-  standalone: true,
-  imports: [],
   templateUrl: './user-update.component.html',
-  styleUrl: './user-update.component.css'
+  styleUrls: ['./user-update.component.css']
 })
-export class UserUpdateComponent {
+export class UserUpdateComponent implements OnInit {
+  id!: number;
+  user: User = new User();
 
+  constructor(private userService: UserService,
+              private route: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.getUserById();
+  }
+
+  updateUser() {
+    this.userService.updateUser(this.id, this.user).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.redirectToUserList();
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    });
+  }
+
+  redirectToUserList() {
+    this.router.navigate(['/users']);
+  }
+
+  onSubmit() {
+    console.log(this.user);
+    this.updateUser();
+  }
+
+  private getUserById() {
+    this.id = this.route.snapshot.params['id'];
+    this.userService.getUserById(this.id).subscribe({
+      next: (data) => {
+        this.user = data;
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    });
+  }
 }
